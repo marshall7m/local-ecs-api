1083321454b56c2a2b595b91dbbd530bfcfa91a8
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-21 23:27:06 -0700

    Create test_main.py

---
+++ tests/integration/test_main.py
@@ -0,0 +1,63 @@
+import pytest
+from fastapi.testclient import TestClient
+from local_ecs_api.main import app
+from moto import mock_ecs
+import boto3
+import os
+
+client = TestClient(app)
+
+@pytest.mark.skip("Not implemented")
+def test_list_tasks():
+    pass
+
+@pytest.mark.skip("Not implemented")
+def test_describe_tasks():
+    pass
+
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_REGION'] = 'us-east-1'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
+@mock_ecs
+def test_run_task(aws_credentials):
+    ecs = boto3.client("ecs")
+    task = ecs.register_task_definition(
+        containerDefinitions=[
+            {
+                'name': 'sleep',
+                'command': [
+                    'sleep',
+                    '5',
+                ],
+                'cpu': 10,
+                'essential': True,
+                'image': 'busybox',
+                'memory': 10,
+            },
+        ],
+        family='sleep5',
+        taskRoleArn='arn:aws:iam::12345679012:role/mock-task',
+    )
+
+    response = client.post(
+        "/RunTask",
+        json={
+            'taskDefinition': task["taskDefinition"]["taskDefinitionArn"]
+        }
+    )
+    print(response.text)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json == {
+        'failures': [],
+        'tasks': [],
+    }
+
