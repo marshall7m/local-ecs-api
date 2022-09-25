e70f89ac44d185951285bcf7856128bde5a3d601
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-24 17:50:48 -0700

    add ECSBackend and RunTaskBackend cls

--- local_ecs_api/models.py
+++ local_ecs_api/models.py
@@ -237,3 +237,161 @@ class DescribeTasksRequest(BaseModel):
 
 class ListTasksRequest(BaseModel):
     pass
+
+
+class RunTaskBackend:
+    """
+    Backend class used for converting local Docker container metadata into
+    ECS compatible response attributes
+    """
+
+    def __init__(self, request: RunTaskRequest, docker_task: DockerTask):
+        self.request = request
+        self.docker_task = docker_task
+        self.metadata = {}
+
+        aws_attr = self._parse_arn(self.docker_task.task_def_arn)
+        self.region = aws_attr["region"]
+        self.account_id = aws_attr["account_id"]
+
+        self.essential_containers = [
+            c["name"]
+            for c in self.docker_task.task_def["containerDefinitions"]
+            if c["essential"] == True
+        ]
+
+    def pull(self):
+        self.containers = self.docker_task.docker.ps()
+
+    def get_task(self):
+        started_at = min([datetime.timestamp(c.created) for c in self.containers])
+        return {
+            # TODO change to docker.compose.ls once
+            # supported: https://github.com/gabrieldemarmiesse/python-on-whales/issues/367
+            "lastStatus": "success",
+            "createdAt": self.docker_task.created_at,
+            "executionStoppedAt": self.get_execution_stopped_at(),
+            "healthStatus": self.get_task_health_status(),
+            # TODO get more precise times for below attributes
+            "pullStartedAt": self.docker_task.created_at,
+            "pullStoppedAt": self.docker_task.created_at,
+            "startedAt": started_at,
+        }
+
+    def get_execution_stopped_at(self):
+        finished_ts = max(
+            [datetime.timestamp(c.state.finished_at) for c in self.containers]
+        )
+
+        # containers that are still running return a negative timestamp
+        if finished_ts < 0:
+            return 0
+        return min(finished_ts)
+
+    def get_task_health_status(self):
+        for c in self.containers:
+            if c.name in self.essential_containers:
+                status = getattr(c.state, "health", "UKNOWN")
+                if status == "healthy":
+                    continue
+                else:
+                    return status
+
+    @staticmethod
+    def _parse_arn(resource_arn):
+        match = re.match(
+            "^arn:aws:ecs:(?P<region>[^:]+):(?P<account_id>[^:]+):(?P<service>[^:]+)/(?P<id>.*)$",
+            resource_arn,
+        )
+        return match.groupdict()
+
+    def get_containers(self) -> List[Containers]:
+        response = []
+
+        for id in self.containers:
+            c = self.docker_task.docker.container.inspect(id)
+            response.append(
+                Containers(
+                    containerArn=f"arn:aws:ecs:{self.region}:{self.account_id}:container/{c.id}",
+                    cpu=c.host_config.cpu_shares,
+                    exitCode=c.state.exit_code,
+                    gpuIds=[],
+                    # TODO find healthstatus attr
+                    healthStatus=c.state.status,
+                    image=c.config.image,
+                    imageDigest=c.image,
+                    lastStatus=c.state.status,
+                    managedAgents=[],
+                    memory=c.host_config.memory,
+                    memoryReservation=c.host_config.memory_reservation,
+                    name=c.name,
+                    # TODO replace empty list with docker mapping
+                    networkBindings=[],
+                    # TODO replace empty list with docker mapping
+                    networkInterfaces=[],
+                    reason=c.state.error,
+                    runtimeId=c.id,
+                    taskArn=f"arn:aws:ecs:{self.region}:{self.account_id}:task/{self.request.cluster}/{self.docker_task.task_name}",
+                )
+            )
+        return response
+
+
+class ECSBackend:
+    def __init__(self, pickle_path):
+        self.tasks = {}
+        self.pickle_path = pickle_path
+
+    def save(self):
+        with open(self.pickle_path, "wb") as f:
+            pickle.dump(self, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def describe_tasks(self, tasks: List[str], include=[]):
+        """
+        Returns ECS DescribeTask response replaced with local docker compose container values
+
+        Arguments:
+            tasks: List of task IDs or ARNs
+            #TODO implement include argument
+            include: List of additional attributes to include (only supports `TAG`)
+        """
+        response = {"tasks": [], "failures": []}
+
+        for t in tasks:
+            task_response = {}
+            # TODO convert any task arn that are passed in tasks before lookup
+            task = self.tasks[t]
+            task.pull()
+            # TODO change to docker.compose.ls once
+            # supported: https://github.com/gabrieldemarmiesse/python-on-whales/issues/367
+            if "success" == "failure":
+                response.failures.append(
+                    Failures(
+                        arn=task.arn,
+                        detail=task.logs,
+                        # TODO: use exception message instead
+                        reason="placeholder-reason",
+                    )
+                )
+
+            response["tasks"].append(
+                Tasks(
+                    containers=task.get_containers(),
+                    **task.get_task(),
+                    **task.request.dict(),
+                )
+            )
+
+        return response
+
+    def run_task(self, task_def_arn, overrides, count):
+        ecs = boto3.client("ecs", endpoint_url=os.environ.get("ECS_ENDPOINT_URL"))
+
+        task_def = ecs.describe_task_definition(taskDefinition=task_def_arn)
+        task = DockerTask(task_def)
+
+        task.create_docker_compose_stack(overrides)
+        log.info("Running docker compose up")
+        task.up(count, detach=True)
+
+        return task
