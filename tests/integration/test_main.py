0ec08e0d52cd773784e3284d15b529c626696035
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-04 17:40:55 -0700

    add runtaskresponse assertions to testclient tests

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,17 +1,27 @@
+import logging
+from pprint import pformat
+
+import pytest
 from fastapi.testclient import TestClient
 from moto import mock_ecs
 import boto3
 from local_ecs_api.main import app
 from tests.data import task_defs
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
 client = TestClient(app)
 
 
 @mock_ecs
 def test_list_tasks(aws_credentials):
+    """
+    Ensures ListTask endpoint returns the expected list of task ARNs
+    """
     ecs = boto3.client("ecs")
     cluster = "test-cluster"
-    task = ecs.register_task_definition("")
+    task = ecs.register_task_definition(**task_defs["fast_success"])
 
     run_task = client.post(
         "/",
@@ -42,6 +52,10 @@ def test_list_tasks(aws_credentials):
 
 @mock_ecs
 def test_describe_tasks_gets_updated_results(aws_credentials):
+    """
+    Ensures DescribeTasks endpoint returns the expected response for filtering
+    task that are expected to succeed
+    """
     ecs = boto3.client("ecs")
     task = ecs.register_task_definition(
         containerDefinitions=[
@@ -73,15 +87,19 @@ def test_describe_tasks_gets_updated_results(aws_credentials):
     assert response.status_code == 200
     response_json = response.json()
 
-    # TODO: create proper expected response
-    assert response_json == {
-        "failures": [],
-        "tasks": [],
-    }
+    assert len(response_json["failures"]) == 0
+    assert len(response_json["tasks"]["containers"]) == len(
+        task["containerDefinitions"]
+    )
 
 
+@pytest.mark.usefixtures("aws_credentials")
 @mock_ecs
-def test_run_task_with_success(aws_credentials):
+def test_run_task_with_success():
+    """
+    Ensures RunTask endpoint returns the expected response for task definitions
+    that are expected to succeed
+    """
     ecs = boto3.client("ecs")
     task = ecs.register_task_definition(**task_defs["fast_success"])
 
@@ -92,9 +110,26 @@ def test_run_task_with_success(aws_credentials):
     )
     assert response.status_code == 200
 
+    data = response.json()
+    log.debug("Response:")
+    log.debug(pformat(data))
 
+    assert len(data["failures"]) == 0
+    assert len(data["tasks"]["containers"]) == len(
+        task_defs["fast_success"]["containerDefinitions"]
+    )
+
+    for c in data["tasks"]["containers"]:
+        assert c["lastStatus"] == "exited"
+
+
+@pytest.mark.usefixtures("aws_credentials")
 @mock_ecs
-def test_run_task_with_failure(aws_credentials):
+def test_run_task_with_failure():
+    """
+    Ensures RunTask endpoint returns the expected response for task definitions
+    that are expected to fail
+    """
     ecs = boto3.client("ecs")
     task = ecs.register_task_definition(**task_defs["fast_fail"])
 
@@ -104,3 +139,36 @@ def test_run_task_with_failure(aws_credentials):
         json={"taskDefinition": task["taskDefinition"]["taskDefinitionArn"]},
     )
     assert response.status_code == 200
+
+    data = response.json()
+    log.debug("Response:")
+    log.debug(pformat(data))
+
+    assert len(data["failures"]) == 1
+
+
+@pytest.mark.usefixtures("aws_credentials")
+@mock_ecs
+def test_run_task_pull_img_failure():
+    """
+    Ensures RunTask endpoint returns the expected response for task definitions
+    that are expected to fail at the task setup level
+    """
+    ecs = boto3.client("ecs")
+    task = ecs.register_task_definition(**task_defs["invalid_img"])
+
+    response = client.post(
+        "/",
+        headers={"x-amz-target": "RunTask"},
+        json={"taskDefinition": task["taskDefinition"]["taskDefinitionArn"]},
+    )
+    assert response.status_code == 200
+
+    data = response.json()
+    log.debug("Response:")
+    log.debug(pformat(data))
+
+    assert len(data["failures"]) == 0
+
+    assert data["tasks"][0]["stopCode"] == "TaskFailedToStart"
+    assert data["tasks"][0]["lastStatus"] == "STOPPED"
