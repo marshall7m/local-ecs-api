230c2052fefa21f873453a2c5117d66cc44a8054
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-07 08:04:14 -0700

    use python:3.9-slim-buster for base img
    sudo is pre-installed to avoid chicken or the egg problem

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 set -x
 apt-get -y update
-apt-get -y install sudo gnupg
+apt-get -y install gnupg
 
 # ecs-cli
 curl -s -Lo /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
 mkdir /tmp/src-tmp
-sudo chmod +rwx /var/lib/apt/lists/*
+chmod +rwx /var/lib/apt/lists/*
 
 cat <<EOF > /tmp/src-tmp/pub_ecs_cli.txt
 -----BEGIN PGP PUBLIC KEY BLOCK-----
@@ -142,14 +142,14 @@ EOF
 gpg --import /tmp/src-tmp/pub_ecs_cli.txt
 curl -s -Lo ecs-cli.asc https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest.asc
 gpg --verify ecs-cli.asc /usr/local/bin/ecs-cli
-sudo chmod +x /usr/local/bin/ecs-cli
+chmod +x /usr/local/bin/ecs-cli
 
 # docker compose plugin
 DOCKER_CONFIG="${DOCKER_CONFIG:-$HOME/.docker}"
 mkdir -p "${DOCKER_CONFIG}"/cli-plugins
 curl -s -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-aarch64 -o "${DOCKER_CONFIG}"/cli-plugins/docker-compose
-sudo chmod +x "${DOCKER_CONFIG}"/cli-plugins/docker-compose
+chmod +x "${DOCKER_CONFIG}"/cli-plugins/docker-compose
 
-sudo apt-get clean
-sudo rm -rf /var/lib/apt/lists/*
-sudo rm -rf /tmp/src-tmp
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/src-tmp
