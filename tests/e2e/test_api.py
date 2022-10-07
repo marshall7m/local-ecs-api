ef7972a7eafe5cac3d68429ee1164ecdb6274f7d
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-07 12:17:15 -0700

    mv docker connect to connect_tests_to_api fixture

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -17,14 +17,21 @@ FILE_DIR = os.path.dirname(__file__)
 
 
 @pytest.fixture(scope="module", autouse=True)
-def local_ecs_api():
-    os.environ["NETWORK_NAME"] = "local-ecs-api-tests"
-    # os.environ["LOCAL_ECS_API_ALIAS"] = "local-ecs"
+def local_api():
     os.environ["LOCAL_ECS_API_ENDPOINT"] = "http://" + "local-ecs-api" + ":8000"
     docker = DockerClient(compose_files=[os.path.join(FILE_DIR, "docker-compose.yml")])
 
     docker.compose.up(build=True, detach=True)
 
+    yield docker
+
+    docker.compose.stop()
+    docker.compose.down()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def connect_tests_to_api(local_api):
+    os.environ["NETWORK_NAME"] = "local-ecs-api-tests"
     # CI env var will be set if running in GitHub Action job
     if not os.environ.get("CI"):
         # needed only if running tests within container
@@ -32,6 +39,7 @@ def local_ecs_api():
         with open("/etc/hostname", "r", encoding="utf-8") as f:
             container_id = f.read().strip()
             try:
+                docker = DockerClient()
                 docker.network.connect(os.environ["NETWORK_NAME"], container_id)
             except DockerException as e:
                 if re.search(r"already exists in network", e.stderr):
@@ -39,14 +47,12 @@ def local_ecs_api():
                 else:
                     raise e
 
-    yield docker
+        yield
 
-    docker.network.disconnect(os.environ["NETWORK_NAME"], container_id)
-    docker.compose.stop()
-    # docker.compose.down()
+        docker.network.disconnect(os.environ["NETWORK_NAME"], container_id)
 
 
-def test_redirect_supported():
+def test_redirect_supported(aws_credentials):
     """Ensures ECS API requests that aren't covered by local API are redirected to target AWS endpoint"""
     ecs = boto3.client("ecs", endpoint_url=os.environ.get("LOCAL_ECS_API_ENDPOINT"))
     response = ecs.list_clusters()
