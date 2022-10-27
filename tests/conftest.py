b5e836a63573f3d340b1979535316e7198f86313
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-10-26 21:44:03 -0700

    update testing aws region

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,5 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_REGION"] = "us-east-1"
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+    os.environ["AWS_REGION"] = "us-west-2"
+    os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
